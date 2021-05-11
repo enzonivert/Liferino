9b0c56f32daf64671e5a74052cc88d028d880c14
enzo nivert <enzo.nivert@epitech.eu>
2021-05-11 18:26:58 +0200

    switch api to another repo

--- api/routes/testAPI.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-/* GET test page */
-router.get("/", function(req, res, next) {
-    res.send("William rend moi mes 25 bales");
-});
-
-module.exports = router;
--- src/App.js
+++ src/App.js
@@ -8,22 +8,13 @@ class App extends Component {
     this.state = { apiResponse: "" };
 }
 
-callAPI() {
-    fetch("http://localhost:9000/testAPI")
-        .then(res => res.text())
-        .then(res => this.setState({ apiResponse: res }));
-}
-
-componentWillMount() {
-    this.callAPI();
-}
   render () {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p className="App-intro">
-            {this.state.apiResponse}
+            {"William rend mon fric"}
           </p>
         </header>
       </div>
